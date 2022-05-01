9ed3f4bffdb5802af46a3d4e34b54b45c4fe5ff0
Rajkumar <rajkumaar.rajendran@gmail.com>
2022-05-01 22:05:14 +0100

    Comments added for the startup.cs

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -25,6 +25,7 @@ namespace WebApp.UnderTheHood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Cookie Authentication implementation
             services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
             {
                 options.Cookie.Name = "MyCookieAuth";
